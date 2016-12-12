92892454f5989f406ea6a0849d5f8ced7d45e87b
johnsmith444 <bertrand.valentin@promsocatc.net>
2016-12-12 20:01:23 +0000

    premier commit
    yes, first one...

---
+++ ex1/up.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Montitre</title>
+	<link rel="stylesheet" href="./css/page.css">
+</head>
+<body>
+	<header id="header" class="header">
+		<H1>
+			Collège Technique "Aumôniers du Travail" <br>
+			Enseignement de Promotion Sociale<br>
+		</H1>
+		<ul>
+			<li><a href="index.php" >Acceuil</a></li>
+			<li><a href="contact.php" >Contact</a></li>
+		</ul>
+	</header>
